ad0828a5774e7abfade1cad610c3d029432fe1a0
Kornelia <ez.kornelia@gmail.com>
2016-10-17 13:23:36 +0100

    Adding chapter 7, ex1

---
+++ chap07/ex1.rb
@@ -0,0 +1,21 @@
+#Chris Pine Learn to Program book (Ruby), Chapter 7.
+
+#1. Comparison methods
+
+puts 1 < 2
+puts 1 > 2
+
+puts 5 >= 5
+puts 5 <= 4
+
+puts 1 == 1
+puts 2 != 2
+
+puts "cat" < "dog" #lexicographical comparison
+#this will return true or false
+
+puts "bug lady" < "Xander"
+puts "bug lady".downcase < "Xander".downcase
+
+puts 2 < 10
+puts "2" < "10" #false!!
