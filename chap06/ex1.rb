5104833a5633203837105eaa23ebe97ad1faf7b7
Kornelia <ez.kornelia@gmail.com>
2016-10-17 11:05:32 +0100

    Adding chapter 6

---
+++ chap06/ex1.rb
@@ -0,0 +1,36 @@
+#Chris Pine, chapter 6
+
+var1 = "stop"
+var2 = "deliver repaid desserts"
+var3 = "....TCELES B HSUP A magis spell?"
+
+puts var1.reverse
+puts var2.reverse
+puts var3.reverse
+puts var1
+puts var2
+puts var3
+
+
+letters = "AaabCCbBBaaaBCAbca"
+puts letters.upcase
+puts letters.downcase
+puts letters.swapcase
+puts letters.capitalize
+puts " a".capitalize
+puts letters
+
+
+line_width = 40
+puts ("Old Mother Hubbard".center(line_width))
+puts ("Sat in her cupboard".center(line_width))
+puts ("Eating her curds and whey,".center(line_width))
+puts ("When along came a spider".center(line_width))
+puts ("Who sat down beside her".center(line_width))
+puts ("And scared her poor show dog away.".center(line_width))
+
+str = "--> text <--"
+puts (str.ljust(line_width))
+puts (str.center(line_width))
+puts (str.rjust(line_width))
+puts (str.ljust(line_width / 2) + str.rjust(line_width / 2))
