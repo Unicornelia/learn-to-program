8cf8532af1a6de054967e598a6aa5a5d00df582f
Kornelia <ez.kornelia@gmail.com>
2016-10-17 12:49:42 +0100

    Adding the random number task

---
+++ chap06/ex5.rb
@@ -0,0 +1,25 @@
+#Random number calculator
+
+puts rand
+puts rand
+puts rand
+puts (rand(100))
+puts (rand(100))
+puts (rand(100))
+puts (rand(1))
+puts (rand(1))
+puts (rand(1))
+
+puts "The weatherman said there is a " + rand(101).to_s + "% chance of rain, \nbut you can never trust a weatherman."
+
+srand 678
+puts(rand(100))
+puts(rand(100))
+puts(rand(100))
+puts(rand(100))
+puts ""
+srand 678
+puts(rand(100))
+puts(rand(100))
+puts(rand(100))
+puts(rand(100))
