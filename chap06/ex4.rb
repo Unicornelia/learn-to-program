ead55fd5e2db230e0551f35bf0d7767f9473ead2
Kornelia <ez.kornelia@gmail.com>
2016-10-17 11:29:52 +0100

    Uploading the 4th task from chapter 6

---
+++ chap06/ex4.rb
@@ -0,0 +1,11 @@
+#Chris Pine Learn to Program book (Ruby), Chapter 6.4.
+#More Arithmetic - not my fav but hey
+
+puts 5**2
+puts 5**0.5
+puts 7/3
+puts 7%3
+puts 365%7
+
+puts (5-2).abs
+puts (2-5).abs
