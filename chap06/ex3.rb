0c36f1b8ca844b161a0c4b51316ca657de441693
Kornelia <ez.kornelia@gmail.com>
2016-10-17 11:20:10 +0100

    Adding the table of content task from chapter 6

---
+++ chap06/ex3.rb
@@ -0,0 +1,13 @@
+#Table of Contents
+#Chris Pine book, chapter 6, task
+
+
+line_width = 20
+
+puts " "
+puts ("Table of Contents".center(line_width * 2.5))
+puts ""
+puts ("Chapter 1:  Getting Started".ljust(line_width / 2) + "page 1".rjust(line_width))
+puts ("Chapter 2:  Numbers".ljust(line_width / 2) + "page 9".rjust(line_width + 8))
+puts ("Chapter 3:  Letters".ljust(line_width / 2) + "page 13".rjust(line_width + 9))
+puts " "
